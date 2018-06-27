254d3d2dca1d827dd6b34b3c07125717b68d7aca
Laurent Laborde <llaborde@bottomline.com>
2018-06-27 17:09:30 +0100

    Updated user management: delete in keycloak is implemented

--- plex-app/src/main/java/org/lolobored/plex/spring/services/impl/UserServiceImpl.java
+++ plex-app/src/main/java/org/lolobored/plex/spring/services/impl/UserServiceImpl.java
@@ -10,10 +10,10 @@ import org.keycloak.representations.idm.UserRepresentation;
 import org.lolobored.http.HttpException;
 import org.lolobored.plex.PlexService;
 import org.lolobored.plex.spring.config.KeycloakConfig;
-import org.lolobored.plex.spring.tasks.ESLoaderTask;
 import org.lolobored.plex.spring.models.User;
 import org.lolobored.plex.spring.repository.UserRepository;
 import org.lolobored.plex.spring.services.UserService;
+import org.lolobored.plex.spring.tasks.ESLoaderTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -51,16 +51,18 @@ public class UserServiceImpl implements UserService {
 		}
 		user.setPlexToken(plexToken);
 		// create user in keycloak
-		String id= createUserInKeycloak(authToken, user);
+		String id = createUserInKeycloak(authToken, user);
 		user.setId(id);
 		// update user in keycloak
 		updateUserInKeycloak(authToken, user);
-		User newUser= repository.save(user);
+		User newUser = repository.save(user);
 		return newUser;
 	}
 
 	@Override
 	public void deleteUser(String authToken, String id) {
+		// update user in keycloak
+		deleteUserInKeycloak(authToken, id);
 		repository.deleteById(id);
 	}
 
@@ -78,7 +80,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUser(String username) {
 		Optional<User> user = repository.findByUserName(username);
-		if (!user.isPresent()){
+		if (!user.isPresent()) {
 			User newuser = new User();
 			newuser.setUserName(username);
 			repository.save(newuser);
@@ -102,16 +104,16 @@ public class UserServiceImpl implements UserService {
 		user.setPlexToken(plexToken);
 		// update user in keycloak
 		updateUserInKeycloak(authToken, user);
-		User newUser= repository.save(user);
+		User newUser = repository.save(user);
 		return newUser;
 	}
 
-	private String createUserInKeycloak(String authToken, User user){
+	private String createUserInKeycloak(String authToken, User user) {
 		Keycloak kc = Keycloak.getInstance(
-			keycloakConfig.getAuthServerUrl(),
-			keycloakConfig.getRealm(), // the realm to log in to
-			keycloakConfig.getResource(),
-			authToken);
+				keycloakConfig.getAuthServerUrl(),
+				keycloakConfig.getRealm(), // the realm to log in to
+				keycloakConfig.getResource(),
+				authToken);
 		// Get realm
 		RealmResource realmResource = kc.realm(keycloakConfig.getRealm());
 		UsersResource usersResource = realmResource.users();
@@ -122,16 +124,16 @@ public class UserServiceImpl implements UserService {
 
 		UserRepresentation userRepresentation = new UserRepresentation();
 		userRepresentation.setUsername(user.getUserName());
-		Response response= usersResource.create(userRepresentation);
+		Response response = usersResource.create(userRepresentation);
 		return response.getLocation().getPath().replaceAll(".*/([^/]+)$", "$1");
 	}
 
-	private void updateUserInKeycloak(String authToken, User user){
+	private void updateUserInKeycloak(String authToken, User user) {
 		Keycloak kc = Keycloak.getInstance(
-			keycloakConfig.getAuthServerUrl(),
-			keycloakConfig.getRealm(), // the realm to log in to
-			keycloakConfig.getResource(),
-			authToken);
+				keycloakConfig.getAuthServerUrl(),
+				keycloakConfig.getRealm(), // the realm to log in to
+				keycloakConfig.getResource(),
+				authToken);
 		// Get realm
 		RealmResource realmResource = kc.realm(keycloakConfig.getRealm());
 		UsersResource usersResource = realmResource.users();
@@ -142,27 +144,41 @@ public class UserServiceImpl implements UserService {
 		credential.setTemporary(false);
 
 		// retrieve user in keycloak
-		UserRepresentation userRepresentation= usersResource.get(user.getId()).toRepresentation();
+		UserRepresentation userRepresentation = usersResource.get(user.getId()).toRepresentation();
 		userRepresentation.setFirstName(user.getFirstName());
 		userRepresentation.setLastName(user.getLastName());
 		userRepresentation.setEnabled(true);
 		userRepresentation.setCredentials(Arrays.asList(credential));
 		// get the user resource
-		UserResource userResource= usersResource.get(user.getId());
+		UserResource userResource = usersResource.get(user.getId());
 		userResource.update(userRepresentation);
 		userResource.resetPassword(credential);
 
 		// Get realm role "tester" (requires view-realm role)
 		RoleRepresentation adminRole = kc.realm("plex").roles()
-			.get("admin").toRepresentation();
+				.get("admin").toRepresentation();
 
-		if (user.isAdmin()){
+		if (user.isAdmin()) {
 			usersResource.get(user.getId()).roles().realmLevel()
-				.add(Arrays.asList(adminRole));
-		}
-		else{
+					.add(Arrays.asList(adminRole));
+		} else {
 			usersResource.get(user.getId()).roles().realmLevel()
-				.remove(Arrays.asList(adminRole));
+					.remove(Arrays.asList(adminRole));
 		}
+	}
+
+	private void deleteUserInKeycloak(String authToken, String id) {
+		Keycloak kc = Keycloak.getInstance(
+				keycloakConfig.getAuthServerUrl(),
+				keycloakConfig.getRealm(), // the realm to log in to
+				keycloakConfig.getResource(),
+				authToken);
+		// Get realm
+		RealmResource realmResource = kc.realm(keycloakConfig.getRealm());
+		UsersResource usersResource = realmResource.users();
+
+		// get the user resource
+		UserResource userResource = usersResource.get(id);
+		userResource.remove();
 	}
 }
