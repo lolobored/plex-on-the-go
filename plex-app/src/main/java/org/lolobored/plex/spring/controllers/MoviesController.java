7817ba954616632b9b3d71bb5f8640f152d8b106
Laurent Laborde <llaborde@bottomline.com>
2018-07-02 00:59:13 +0100

    Small glitch on search movies

--- plex-app/src/main/java/org/lolobored/plex/spring/controllers/MoviesController.java
+++ plex-app/src/main/java/org/lolobored/plex/spring/controllers/MoviesController.java
@@ -4,7 +4,9 @@ package org.lolobored.plex.spring.controllers;
 import org.lolobored.plex.model.Media;
 import org.lolobored.plex.spring.models.PlexUser;
 import org.lolobored.plex.spring.models.Search;
+import org.lolobored.plex.spring.models.User;
 import org.lolobored.plex.spring.services.MovieService;
+import org.lolobored.plex.spring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +21,22 @@ public class MoviesController {
 
 	@Autowired
 	MovieService movieService;
+	@Autowired
+	UserService userService;
 
 	@GetMapping
 	@PreAuthorize("isAuthenticated()")
 	public List<Media> getMovies(Principal principal) {
-		return movieService.getMovies(principal.getName());
+		User user = userService.getUserById(principal.getName());
+		return movieService.getMovies(user.getUserName());
 
 	}
 
 	@PostMapping(path = {"/search"})
 	@PreAuthorize("isAuthenticated()")
 	public List<Media> searchMovies(@RequestBody Search search, Principal principal) {
-
-		return movieService.searchMovies(principal.getName(), search);
+		User user = userService.getUserById(principal.getName());
+		return movieService.searchMovies(user.getUserName(), search);
 	}
 
 	@GetMapping(path = {"/genre"})
